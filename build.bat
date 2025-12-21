d5eeed0be06e249cd12b5c358c1b697366378338
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-21 14:38:37 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,21 +1,15 @@
 @echo off
 
-net stop WebConsole > nul 2>&1
-erase "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
-erase webconsole.exe > nul 2>&1
-
 echo Checking libraries are installed...
-go get github.com/nfnt/resize
-go get github.com/dennwc/gotrace
-go get github.com/kodeworks/golang-image-ico
-go get golang.org/x/crypto/bcrypt
-go get github.com/xuri/excelize/v2
-go get golang.org/x/crypto/argon2
+rem go get github.com/nfnt/resize
+
+if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
 
 echo Building...
-go build webconsole.go
+go build client.go
+move client.exe ..\RADIUSClient
 
-copy webconsole.exe "C:\Program Files\WebConsole" > nul 2>&1
-xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
-xcopy /E /Y ..\ace-builds\src-noconflict "C:\Program Files\WebConsole\www\ace" > nul 2>&1
-net start WebConsole > nul 2>&1
+rem copy webconsole.exe "C:\Program Files\WebConsole" > nul 2>&1
+rem xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
+rem xcopy /E /Y ..\ace-builds\src-noconflict "C:\Program Files\WebConsole\www\ace" > nul 2>&1
+rem net start WebConsole > nul 2>&1
