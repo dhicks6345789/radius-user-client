0c351330e070fddb1e6b070dd346372b2c8bc6a7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 15:43:47 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,15 @@
 @echo off
 
+source VERSION
+CURRENTDATE=`date +"%d/%m/%Y-%H:%M"`
+BUILDVERSION="$VERSION-local-$CURRENTDATE"
+
 if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
 
 rem Module installation.
 go mod download gopkg.in/yaml.v3
 
 echo Building...
-go build client.go 2>&1
+rem go build client.go 2>&1
+go build -ldflags "-X main.buildVersion=$BUILDVERSION" client.go 2>&1
 move client.exe ..\RADIUSClient
