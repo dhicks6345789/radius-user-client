01b373038ba1db245ecf3022c218a97186beaf51
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 14:30:39 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,15 +1,10 @@
 @echo off
 
 echo Checking libraries are installed...
-rem go get github.com/nfnt/resize
+go get gopkg.in/yaml.v3
 
 if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
 
 echo Building...
 go build client.go 2>&1
 move client.exe ..\RADIUSClient
-
-rem copy webconsole.exe "C:\Program Files\WebConsole" > nul 2>&1
-rem xcopy /E /Y www "C:\Program Files\WebConsole\www" > nul 2>&1
-rem xcopy /E /Y ..\ace-builds\src-noconflict "C:\Program Files\WebConsole\www\ace" > nul 2>&1
-rem net start WebConsole > nul 2>&1
