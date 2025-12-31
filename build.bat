81f734311c17ac4ccceaf0f20dd739d626672e6e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 15:56:18 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ echo The VERSION is: %VERSION%
 
 rem CURRENTDATE=`date +"%d/%m/%Y-%H:%M"`
 for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set dt=%%a
+echo %dt%
 set YYYY=%dt:~0,4%
 set MM=%dt:~4,2%
 set DD=%dt:~6,2%
@@ -19,9 +20,10 @@ rem BUILDVERSION="$VERSION-local-$CURRENTDATE"
 if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
 
 rem Module installation.
-go mod download gopkg.in/yaml.v3
+go mod download gopkg.in/yaml.v3 2>&1
 
 echo Building...
 rem go build client.go 2>&1
 rem go build -ldflags "-X main.buildVersion=$BUILDVERSION" client.go 2>&1
-move client.exe ..\RADIUSClient
+erase ..\RADIUSClient\client.exe 2>&1
+move client.exe ..\RADIUSClient 2>&1
