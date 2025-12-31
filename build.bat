d1894c836752db39aa42b042c086ee222fc0a211
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 14:49:30 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,6 +2,9 @@
 
 if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
 
+rem Module installation.
+go mod download gopkg.in/yaml.v3
+
 echo Building...
 go build client.go 2>&1
 move client.exe ..\RADIUSClient
