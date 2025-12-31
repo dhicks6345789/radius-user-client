b313b0a5db980ec40286f200a032030351eef497
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 17:06:34 +0000

    Update client.go

--- client.go
+++ client.go
@@ -71,8 +71,9 @@ func getCurrentUser() string {
 	if strings.HasPrefix(queryResult, "No User exists for") {
 		fmt.Println("To do: figure out what to do if no user reported.")
 	} else {
-		fmt.Printf("%q\n", strings.Fields(queryResult))
-		username = strings.TrimSpace(strings.Split(queryResult, " ")[2])
+		// To do: more actual parsing goes here to get the current username from a possible list of several.
+		// fmt.Printf("%q\n", strings.Fields(queryResult))
+		username = strings.Fields(queryResult)[8])
 	}
 	return username
 }
