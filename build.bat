e2977740cb76d36e3a7becea3c851091dbf54ae8
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 16:04:48 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+
 rem Get the version number from the repository...
 set /p VERSION=<VERSION
 rem ...figure out the current date and time...
@@ -8,16 +9,16 @@ set MM=%dt:~4,2%
 set DD=%dt:~6,2%
 set HH=%dt:~8,2%
 set NN=%dt:~10,2%
+set CURRENTDATE=%DD%/%MM%/%YYYY%-%HH%:%NN%
 rem ... and combine those to give a build version value.
 set BUILDVERSION=%VERSION%-local-%CURRENTDATE%
-echo %BUILDVERSION%
 
 if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
 
 rem Module installation.
 go mod download gopkg.in/yaml.v3 2>&1
 
-echo Building...
+echo Building version: %BUILDVERSION%...
 rem go build client.go 2>&1
 rem go build -ldflags "-X main.buildVersion=$BUILDVERSION" client.go 2>&1
 erase ..\RADIUSClient\client.exe 2>&1
