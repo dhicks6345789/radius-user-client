1748f775b459cffdaf93caac42f7f10a7a4e6940
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 16:44:19 +0000

    Update client.go

--- client.go
+++ client.go
@@ -66,6 +66,8 @@ func getCurrentUser() string {
 	queryCmd := exec.Command("cmd", "/C", "query user")
 	queryOut, queryErr := queryCmd.CombinedOutput()
 	if queryErr != nil {
+		fmt.Println(queryOut)
+		fmt.Println(queryErr)
 		log.Fatalf("Error running query user command: %v", queryErr)
 	}
 	return strings.TrimSpace(string(queryOut))
