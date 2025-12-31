5d5ff9b51c087eee32981d6d8e5472ae1d562450
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-12-31 15:45:55 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ rem source VERSION
 rem CURRENTDATE=`date +"%d/%m/%Y-%H:%M"`
 rem BUILDVERSION="$VERSION-local-$CURRENTDATE"
 
-for /f "tokens=2 delims==" %%a in ('findstr "version" VERSION') do set VERSION=%%a
+for /f "tokens=2 delims==" %%a in ('findstr "VERSION" VERSION') do set VERSION=%%a
 echo The VERSION is: %VERSION%
 
 if not exist "..\RADIUSClient" mkdir "..\RADIUSClient"
@@ -14,5 +14,5 @@ go mod download gopkg.in/yaml.v3
 
 echo Building...
 rem go build client.go 2>&1
-go build -ldflags "-X main.buildVersion=$BUILDVERSION" client.go 2>&1
+rem go build -ldflags "-X main.buildVersion=$BUILDVERSION" client.go 2>&1
 move client.exe ..\RADIUSClient
