3c3a11ee9b432afa6a5e9b4352a150acdca8a628
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-03 14:14:59 +0000

    Update client.go

--- client.go
+++ client.go
@@ -106,10 +106,11 @@ func sendAccountingPacket(serverAddr string, secret string, username string, IPA
 	
 	// Set accounting attributes.
 	rfc2866.AcctStatusType_Add(packet, statusType)
-	rfc2866.AcctSessionID_SetString(packet, "unique-session-id-123")
 	rfc2865.UserName_SetString(packet, username)
-	rfc2865.NASIdentifier_SetString(packet, "nas-device-1")
-	rfc2865.NASIPAddress_Set(packet, net.ParseIP(IPAddress))
+	rfc2865.FramedIPAddress_Add(net.ParseIP(IPAddress))
+	//rfc2866.AcctSessionID_SetString(packet, "unique-session-id-123")
+	//rfc2865.NASIdentifier_SetString(packet, "nas-device-1")
+	
 
 	// 3. Exchange the packet with the server - waits for a response.
 	response, err := radius.Exchange(context.Background(), packet, serverAddr)
