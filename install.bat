d6a714827bd1850231f1173157d2ca0d0a0c5695
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-05 13:01:13 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -24,6 +24,7 @@ mkdir "C:\Program Files\RADIUSUserClient" >nul 2>&1
 
 rem Copy the executable and config file.
 copy RADIUSUserClient-win64.exe "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" >nul 2>&1
+copy NSSM\2.24\win64\nssm.exe "C:\Program Files\RADIUSUserClient" >nul 2>&1
 copy config.txt "C:\Program Files\RADIUSUserClient" >nul 2>&1
 
 rem Set permissions on the config file so that only local admin accounts can read it.
@@ -33,10 +34,10 @@ icacls "C:\Program Files\RADIUSUserClient\config.txt" /remove:g * /T /C
 icacls "C:\Program Files\RADIUSUserClient\config.txt" /grant Administrators:(F) /T /C
 
 echo Setting up RADIUSUserClient as a Windows service...
-NSSM\2.24\win64\nssm.exe install RADIUSUserClient "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" >nul 2>&1
-NSSM\2.24\win64\nssm.exe set RADIUSUserClient DisplayName "RADIUS User Client" >nul 2>&1
-NSSM\2.24\win64\nssm.exe set RADIUSUserClient AppNoConsole 1 >nul 2>&1
-NSSM\2.24\win64\nssm.exe set RADIUSUserClient Start SERVICE_AUTO_START >nul 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" install RADIUSUserClient "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" >nul 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient DisplayName "RADIUS User Client" >nul 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient AppNoConsole 1 >nul 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient Start SERVICE_AUTO_START >nul 2>&1
 net start RADIUSUserClient
 
 echo Done.
