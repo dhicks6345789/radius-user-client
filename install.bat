44b6cd3a0cc69705dd3e3819fc3c87cb44c92c85
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-05 13:17:30 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -18,7 +18,7 @@ goto paramLoop
 
 rem Stop / remove any existing running service.
 net stop RADIUSUserClient >nul 2>&1
-nssm remove RADIUSUserClient
+nssm remove RADIUSUserClient confirm
 
 rem Make sure the install folder exists.
 mkdir "C:\Program Files\RADIUSUserClient" >nul 2>&1
