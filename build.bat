711ccafc339a1bb881b5d99ae0625b42a8e032ee
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-05 09:54:34 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -19,11 +19,11 @@ set CURRENTDATE=%DD%/%MM%/%YYYY%-%HH%:%NN%
 rem ... and combine those to give a build version value.
 set BUILDVERSION=%VERSION%-%BUILDNAME%-%CURRENTDATE%
 
-mkdir "..\RADIUSClient" >nul 2>&1
-mkdir "..\RADIUSClient\NSSM" >nul 2>&1
-mkdir "..\RADIUSClient\NSSM\2.24" >nul 2>&1
-mkdir "..\RADIUSClient\NSSM\2.24\win32" >nul 2>&1
-mkdir "..\RADIUSClient\NSSM\2.24\win64" >nul 2>&1
+mkdir "..\RADIUSUserClient" >nul 2>&1
+mkdir "..\RADIUSUserClient\NSSM" >nul 2>&1
+mkdir "..\RADIUSUserClient\NSSM\2.24" >nul 2>&1
+mkdir "..\RADIUSUserClient\NSSM\2.24\win32" >nul 2>&1
+mkdir "..\RADIUSUserClient\NSSM\2.24\win64" >nul 2>&1
 
 rem The Go YAML library.
 go get -u gopkg.in/yaml.v3 2>&1
@@ -31,32 +31,32 @@ rem The Go RADIUS library.
 go get -u layeh.com/radius 2>&1
 
 rem Clear out previous builds.
-erase ..\RADIUSClient\*.exe >nul 2>&1
+erase ..\RADIUSUserClient\*.exe >nul 2>&1
 erase ..\*.zip >nul 2>&1
 if "%BUILDNAME%"=="main" (
-  erase ..\RADIUSClient\config.txt >nul 2>&1
+  erase ..\RADIUSUserClient\config.txt >nul 2>&1
 )
 
 rem Copy over an example config file, but only if the user hasn't already provided their own.
-if not exist ..\RADIUSClient\config.txt (
+if not exist ..\RADIUSUserClient\config.txt (
   echo Existing config file not found - copying default example version over.
-  copy config-example.txt ..\RADIUSClient\config.txt
+  copy config-example.txt ..\RADIUSUserClient\config.txt
 )
 
 rem Copy over the install scripts, including NSSM for Windows Service creation.
-copy install.bat ..\RADIUSClient >nul 2>&1
-copy NSSM\2.24\win32\nssm.exe ..\RADIUSClient\NSSM\2.24\win32 >nul 2>&1
-copy NSSM\2.24\win64\nssm.exe ..\RADIUSClient\NSSM\2.24\win64 >nul 2>&1
+copy install.bat ..\RADIUSUserClient >nul 2>&1
+copy NSSM\2.24\win32\nssm.exe ..\RADIUSUserClient\NSSM\2.24\win32 >nul 2>&1
+copy NSSM\2.24\win64\nssm.exe ..\RADIUSUserClient\NSSM\2.24\win64 >nul 2>&1
 
 echo Building version: %BUILDVERSION%...
 go build -ldflags "-X main.buildVersion=%BUILDVERSION%" client.go 2>&1
 
 if exist client.exe (
   echo Build succesful - creating Zip archive...
-  move client.exe ..\RADIUSClient\RADIUSUserClient-win64.exe >nul 2>&1
-  set ZIPNAME=RADIUSClient-!BUILDNAME!
+  move client.exe ..\RADIUSUserClient\RADIUSUserClient-win64.exe >nul 2>&1
+  set ZIPNAME=RADIUSUserClient-!BUILDNAME!
   if "%BUILDNAME%"=="main" (
-    set ZIPNAME=RADIUSClient
+    set ZIPNAME=RADIUSUserClient
   )
-  tar -a -c -f "..\!ZIPNAME!.zip" ..\RADIUSClient >nul 2>&1
+  tar -a -c -f "..\!ZIPNAME!.zip" ..\RADIUSUserClient >nul 2>&1
 )
