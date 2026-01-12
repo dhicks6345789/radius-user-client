5fc528511f3e403b1127861853847cc08ab39f30
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-12 16:49:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ BINARY=linux-amd64
 if [ "$ARCH" = "arm64" ]; then
   echo "Doing MacOS install..."
   cp RADIUSUserClient-mac-arm64 /usr/local/bin/RADIUSUserClient
-  mkdir "/Library/Application Support/RADIUSUserClient"
+  mkdir "/Library/Application Support/RADIUSUserClient" > /dev/null 2>&1
   cp config.txt "/Library/Application Support/RADIUSUserClient"
 else
   echo "Doing Linux install..."
-  # cp RADIUSUserClient-lin-amd64 /usr/local/bin/RADIUSUserClient
+  cp RADIUSUserClient-lin-amd64 /usr/local/bin/RADIUSUserClient
 fi
 chmod u+x /usr/local/bin/RADIUSUserClient
 
