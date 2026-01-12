3e4b84b91f7c6525d3409db6927f96b598742c51
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-12 16:34:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 echo Starting install...
 
-SYSUNAME=`uname`
+# Work out what architecture we are installing on.
+ARCH=$(uname -m)
+BINARY=linux-amd64
+[[ $ARCH == arm* ]] && BINARY=linux-arm32
+[[ $ARCH == aarch64 ]] && BINARY=linux-arm64
 
-if [ "$SYSUNAME" = "Darwin" ]; then
+if [ "$ARCH" = "arm64" ]; then
   echo "On MacOS!"
+  cp RADIUSUserClient-mac-arm64 /usr/local/bin/RADIUSUserClient
 else
-  # Work out what architecture we are installing on.
-  ARCH=$(uname -m)
-  BINARY=linux-amd64
-  [[ $ARCH == arm* ]] && BINARY=linux-arm32
-  [[ $ARCH == aarch64 ]] && BINARY=linux-arm64
-  
   # Copy the executable to the system.
   # cp RADIUSUserClient-lin-amd64 /usr/local/bin/RADIUSUserClient
-  # chmod u+x /usr/local/bin/RADIUSUserClient
 fi
+chmod u+x /usr/local/bin/RADIUSUserClient
