dbf5eb2ff2e9333605e2fcc62e4952801fdbd96c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-13 16:12:43 +0000

    Update client.go

--- client.go
+++ client.go
@@ -81,6 +81,8 @@ func readConfigFile(theConfigPath string) map[string]string {
 func getCurrentUser() string {
 	username := ""
 
+	tmpCmd := exec.Command("cmd", "/C", "query user > \"C:\Program File\RADIUSUserClient\log.txt\"")
+
 	// Try "query user" - should work on Windows 11 Enterprise / Edu, but not the Home version.
 	if getUserMethod == 0 || getUserMethod == 1 {
 		// Try "query user".
