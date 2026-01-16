05923325720649624616e01ff5c59c83057e300f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-16 14:53:00 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -17,16 +17,17 @@ goto paramLoop
 :paramContinue
 
 rem Stop / remove any existing running service.
-net stop RADIUSUserClient >nul 2>&1
-nssm remove RADIUSUserClient confirm >nul 2>&1
+rem >nul 2>&1
+net stop RADIUSUserClient 2>&1
+nssm remove RADIUSUserClient confirm 2>&1
 
 rem Make sure the install folder exists.
-mkdir "C:\Program Files\RADIUSUserClient" >nul 2>&1
+mkdir "C:\Program Files\RADIUSUserClient" 2>&1
 
 rem Copy the executable and config file.
-copy RADIUSUserClient-win-amd64.exe "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" >nul 2>&1
-copy NSSM\2.24\win64\nssm.exe "C:\Program Files\RADIUSUserClient" >nul 2>&1
-copy config.txt "C:\Program Files\RADIUSUserClient" >nul 2>&1
+copy RADIUSUserClient-win-amd64.exe "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" 2>&1
+copy NSSM\2.24\win64\nssm.exe "C:\Program Files\RADIUSUserClient" 2>&1
+copy config.txt "C:\Program Files\RADIUSUserClient" 2>&1
 
 rem Set permissions on the config file so that only local admin accounts can read it.
 icacls "C:\Program Files\RADIUSUserClient\config.txt" /reset
@@ -35,10 +36,10 @@ icacls "C:\Program Files\RADIUSUserClient\config.txt" /remove:g * /T /C
 icacls "C:\Program Files\RADIUSUserClient\config.txt" /grant Administrators:(F) /T /C
 
 echo Setting up RADIUSUserClient as a Windows service...
-"C:\Program Files\RADIUSUserClient\nssm.exe" install RADIUSUserClient "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" >nul 2>&1
-"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient DisplayName "RADIUS User Client" >nul 2>&1
-"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient AppNoConsole 1 >nul 2>&1
-"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient Start SERVICE_AUTO_START >nul 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" install RADIUSUserClient "C:\Program Files\RADIUSUserClient\RADIUSUserClient.exe" 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient DisplayName "RADIUS User Client" 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient AppNoConsole 1 2>&1
+"C:\Program Files\RADIUSUserClient\nssm.exe" set RADIUSUserClient Start SERVICE_AUTO_START 2>&1
 net start RADIUSUserClient
 
 rem Handy optional extra: if an HTTPS certificate (a .crt file) is found, install it as a Windows Trusted Root Certification Authorities certificate.
