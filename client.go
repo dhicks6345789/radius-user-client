71fd1822ce621ebb795b46db86e6e744c6af27e9
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-23 11:20:14 +0000

    Update client.go

--- client.go
+++ client.go
@@ -197,8 +197,8 @@ func sendAccountingPacket(serverAddr string, secret string, username string, IPA
 	// Other filters / gateways might expect different / additional fields - device ID (possibly the device MAC address) or a unique session ID of some sort.
 	rfc2866.AcctStatusType_Add(packet, statusType)
 	
-	var sessionIDHash [32]byte = sha256.Sum256([]byte(username + IPAddress))
-	sessionID := hex.EncodeToString(sessionIDHash[:])
+	//var sessionIDHash [32]byte = sha256.Sum256([]byte(username + IPAddress))
+	sessionID := hex.EncodeToString(sha256.Sum256([]byte(username + IPAddress))[:])
     debug("SessionID: " + sessionID)
 	
 	rfc2866.AcctSessionID_SetString(packet, sessionID)
